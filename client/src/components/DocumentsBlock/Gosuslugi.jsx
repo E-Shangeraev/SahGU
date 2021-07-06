b7b17311ecdff7935458d169885653fd4eed44de
E-Shangeraev <e.shangeraev@gmail.com>
2021-07-06 15:32:12 +0700

    Меняю ссылку на сайт госуслуги

--- client/src/components/DocumentsBlock/Gosuslugi.jsx
+++ client/src/components/DocumentsBlock/Gosuslugi.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
+const url =
+  // eslint-disable-next-line max-len
+  'https://vk.com/away.php?to=https%3A%2F%2Fwww.gosuslugi.ru%2F10077%2F1%2Fform%3Futm_source%3Dls%26utm_medium%3Dbutton%26utm_campaign%3Dvuz%26utm_content%3D10077'
+
 const Gosuslugi = () => (
   <div className="documents-block documents-block--gosuslugi">
     <h3 className="documents-block__title">
       Через личный кабинет на Госуслугах
     </h3>
     <p className="documents-block__text">
-      Зайти на сайт <a href="www.gosuslugi.ru">www.gosuslugi.ru</a>{' '}
+      Зайти на сайт{' '}
+      <a href={url} target="_blank" rel="noreferrer">
+        www.gosuslugi.ru
+      </a>{' '}
       Зарегистрироваться на портале или войти в личный кабинет, если регистрация
       уже есть. Аккаунт на портале должен быть подтвержденный.
     </p>
