ca023cd8f0b4bff0cee1ea25e823709349b262a7
E-Shangeraev <e.shangeraev@gmail.com>
2021-08-11 13:17:04 +0700

    Разрабатываю интерактивные компоненты шагов поступления

--- client/src/components/Step/Step.jsx
+++ client/src/components/Step/Step.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import './Step.scss'
 
-const Step = ({ item }) => (
-  <li className="step">
-    <div className="step__number">01</div>
-  </li>
-)
+const Step = ({ item, number }) => {
+  const textRef = useRef()
+  const [open, setOpen] = useState(false)
+
+  const handleToggleOpen = () => setOpen(!open)
+
+  useEffect(() => {
+    if (open) {
+      textRef.current.style.maxHeight = `
+      ${textRef.current.scrollHeight}px
+      `
+    } else {
+      textRef.current.style.maxHeight = ''
+    }
+  }, [open])
+
+  return (
+    <li className="step">
+      <div className="step__container">
+        <div className="step__number">
+          <div className="step__number-border">
+            {number < 10 ? `0${number}` : number}
+          </div>
+        </div>
+        <div>
+          <h3 className="step__title">{item.title}</h3>
+          <h4 className="step__subtitle">{item.subtitle}</h4>
+          <p className="step__text" ref={textRef}>
+            {item.text}
+          </p>
+          <button
+            type="button"
+            className={classNames('step__button', { active: open })}
+            onClick={handleToggleOpen}>
+            Подробнее
+          </button>
+        </div>
+      </div>
+    </li>
+  )
+}
 
 Step.propTypes = {
   item: PropTypes.objectOf(PropTypes.any).isRequired,
+  number: PropTypes.number.isRequired,
 }
 
 export default Step
--- client/src/components/Steps/Steps.jsx
+++ client/src/components/Steps/Steps.jsx
@@ -9,8 +9,8 @@ const Steps = ({ qualification, url }) => {
   const [steps, setSteps] = useState([])
 
   useEffect(async () => {
-    const data = await axios.get(url)
-    setSteps(data)
+    // const data = await axios.get(url)
+    // setSteps(data)
   }, [])
 
   return (
@@ -24,19 +24,45 @@ const Steps = ({ qualification, url }) => {
           </h2>
           <ul className="steps__list">
             <Step
+              number={1}
               item={{
                 title: 'Изучить',
                 subtitle:
                   'образовательные программы, которые есть в университете',
-                text: 'Самое волнительное во всем процессе поступления – это, конечно же, сдача вступительных испытаний. Именно от успешного завершения этого этапа зависит то, на какую программу и в какой вуз вы можете поступить. Чем выше планка баллов, установленная университетом, тем качественнее образование, которое он предоставляет тем талантливым студентам, которые выдержали даже самый серьезный конкурс.',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, recusandae! Quia ullam ab, pariatur repellat, facilis temporibus itaque assumenda commodi laborum labore dignissimos corrupti magnam mollitia nihil eaque libero ex quisquam, ut molestiae? Inventore, minus odio architecto aperiam cum voluptas vel magnam, ex dolor accusantium saepe officia voluptatem nulla sit!',
               }}
             />
             <Step
+              number={2}
               item={{
                 title: 'Сдать ЕГЭ',
                 subtitle:
                   'необходимые для поступления на понравившуюся специальность',
                 text: 'Самое волнительное во всем процессе поступления – это, конечно же, сдача вступительных испытаний. Именно от успешного завершения этого этапа зависит то, на какую программу и в какой вуз вы можете поступить. Чем выше планка баллов, установленная университетом, тем качественнее образование, которое он предоставляет тем талантливым студентам, которые выдержали даже самый серьезный конкурс.',
+              }}
+            />
+            <Step
+              number={3}
+              item={{
+                title: 'Подать',
+                subtitle: 'документы согласно правилам приема в университет',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, recusandae! Quia ullam ab, pariatur repellat, facilis temporibus itaque assumenda commodi laborum labore dignissimos corrupti magnam mollitia nihil eaque libero ex quisquam, ut molestiae? Inventore, minus odio architecto aperiam cum voluptas vel magnam, ex dolor accusantium saepe officia voluptatem nulla sit!',
+              }}
+            />
+            <Step
+              number={4}
+              item={{
+                title: 'Начать',
+                subtitle: 'свой путь становления профессионалом',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, recusandae! Quia ullam ab, pariatur repellat, facilis temporibus itaque assumenda commodi laborum labore dignissimos corrupti magnam mollitia nihil eaque libero ex quisquam, ut molestiae? Inventore, minus odio architecto aperiam cum voluptas vel magnam, ex dolor accusantium saepe officia voluptatem nulla sit!',
+              }}
+            />
+            <Step
+              number={5}
+              item={{
+                title: 'Работать',
+                subtitle: 'по специальности, на которую вы обучились в СахГУ',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, recusandae! Quia ullam ab, pariatur repellat, facilis temporibus itaque assumenda commodi laborum labore dignissimos corrupti magnam mollitia nihil eaque libero ex quisquam, ut molestiae? Inventore, minus odio architecto aperiam cum voluptas vel magnam, ex dolor accusantium saepe officia voluptatem nulla sit!',
               }}
             />
           </ul>
