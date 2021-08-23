08757a58c2ccfc615b74318ed542e459f687de39
E-Shangeraev <e.shangeraev@gmail.com>
2021-08-23 12:05:32 +0700

    Переделываю блок новостей под слайдер новостей

--- client/src/components/NewsSlider/NewsSlider.jsx
+++ client/src/components/NewsSlider/NewsSlider.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect, useState } from 'react'
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import SlickSlider from 'react-slick'
 import { v4 as uuidv4 } from 'uuid'
 import axios from 'axios'
 import { Markup } from 'interweave'
+import Button from '@components/Button/Button'
 import './NewsSlider.scss'
 
 const AWS_URL = 'https://sakhgu-images.s3.eu-central-1.amazonaws.com/'
@@ -11,6 +14,39 @@ const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' }
 
 const NewsSlider = () => {
   const [news, setNews] = useState([])
+  const sliderRef = useRef()
+
+  const next = () => {
+    sliderRef.current.slickNext()
+  }
+  const prev = () => {
+    sliderRef.current.slickPrev()
+  }
+
+  const settings = {
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    accessibility: true,
+    arrows: false,
+    adaptiveHeight: false,
+    infinite: false,
+    swipe: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  }
 
   useEffect(async () => {
     const fetchedData = await axios.get('/api/news').then(({ data }) => data)
@@ -22,11 +58,18 @@ const NewsSlider = () => {
       <div className="wrapper">
         <div className="news__top">
           <h2 className="title">Новости</h2>
+          <div className="news__buttons">
+            <Button onClick={prev} outlined />
+            <Button onClick={next} outlined />
+          </div>
         </div>
         {news.length ? (
-          <ul className="news__container">
+          <SlickSlider
+            className="news__container"
+            {...settings}
+            ref={sliderRef}>
             {news.map(item => (
-              <li key={uuidv4()} className="news__preview">
+              <div key={uuidv4()} className="news__preview">
                 <Link to={`/news/${item._id}`}>
                   <div className="news__image">
                     {item.uploadedFile && (
@@ -48,9 +91,9 @@ const NewsSlider = () => {
                     </div>
                   </div>
                 </Link>
-              </li>
+              </div>
             ))}
-          </ul>
+          </SlickSlider>
         ) : null}
       </div>
     </section>
