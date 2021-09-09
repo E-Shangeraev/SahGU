bd7651645b02f459943b4827a8c276393f055ee0
E-Shangeraev <e.shangeraev@gmail.com>
2021-09-09 19:04:17 +0700

    Вывожу контактную информацию на главную страницу из БД

--- client/src/components/Contacts/Contacts.jsx
+++ client/src/components/Contacts/Contacts.jsx
@@ -1,61 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 import './Contacts.scss'
 
-import contacts from '@assets/img/contacts.jpg'
+import contactsImage from '@assets/img/contacts.jpg'
 import phone from '@assets/img/icons/phone.svg'
 import letter from '@assets/img/icons/letter.svg'
 import world from '@assets/img/icons/world.svg'
 import mark from '@assets/img/icons/mark.svg'
 
-const Contacts = ({ id }) => (
-  <section className="contacts" id={id}>
-    <div className="wrapper">
-      <div className="contacts__container">
-        <h2 className="title contacts__title">Контакты</h2>
-        <img src={contacts} alt="Фотография главного входа СахГУ" />
-        <div>
-          <p className="big-text contacts__text">
-            Вы можете связаться с приёмной комиссией любым удобным способом
-          </p>
-          <ul className="contacts__list">
-            <li>
-              <img src={phone} alt="Телефон" />
-              <a href="tel:8 (4242) 45−03−00">8 (4242) 45−03−00</a>
-            </li>
-            <li>
-              <img src={letter} alt="Почта" />
-              <a href="mailto:pk@sakhgu.ru">pk@sakhgu.ru</a>
-            </li>
-            <li>
-              <img src={world} alt="Официальный сайт" />
-              <div>
-                <a href="http://sakhgu.ru" target="_blank" rel="noreferrer">
-                  sakhgu.ru
-                </a>
-                <a
-                  href="http://sakhgu.ru/abitur/"
-                  target="_blank"
-                  rel="noreferrer">
-                  sakhgu.ru/abitur/
-                </a>
-              </div>
-            </li>
-            <li>
-              <img src={mark} alt="Адрес" />
-              <span>
-                693008, Сахалинская область,
-                <br />
-                г. Южно-Сахалинск, Ленина 290, каб. 13
-              </span>
-            </li>
-          </ul>
+const Contacts = ({ id }) => {
+  const [contacts, setContacts] = useState(null)
+
+  useEffect(() => {
+    fetch('/api/contacts/main')
+      .then(response => response.json())
+      .then(data => setContacts(data))
+  }, [])
+
+  return (
+    <section className="contacts" id={id}>
+      <div className="wrapper">
+        <div className="contacts__container">
+          <h2 className="title contacts__title">Контакты</h2>
+          <img src={contactsImage} alt="Фотография главного входа СахГУ" />
+          <div>
+            <p className="big-text contacts__text">
+              Вы можете связаться с приёмной комиссией любым удобным способом
+            </p>
+            {contacts && (
+              <ul className="contacts__list">
+                <li>
+                  <img src={phone} alt="Телефон" />
+                  <a href={`tel:${contacts.phone}`}>{contacts.phone}</a>
+                </li>
+                <li>
+                  <img src={letter} alt="Почта" />
+                  <a href={`mailto:${contacts.email}`}>{contacts.email}</a>
+                </li>
+                <li>
+                  <img src={world} alt="Официальный сайт" />
+                  <div>
+                    {contacts.sites.length > 0 &&
+                      contacts.sites.map(site => (
+                        <a href={site} target="_blank" rel="noreferrer">
+                          {site}
+                        </a>
+                      ))}
+                  </div>
+                </li>
+                <li>
+                  <img src={mark} alt="Адрес" />
+                  {contacts.address}
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 Contacts.propTypes = {
   id: PropTypes.number.isRequired,
