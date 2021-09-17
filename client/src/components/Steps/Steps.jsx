ec7db90e7e0be414ec6f3b188a8c1ae159b22086
E-Shangeraev <e.shangeraev@gmail.com>
2021-09-17 14:22:18 +0700

    Добавляю параллакс-элемент в Шаги поступления

--- client/src/components/Steps/Steps.jsx
+++ client/src/components/Steps/Steps.jsx
@@ -1,7 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react'
+import { Parallax } from 'react-parallax'
+import ScrollAnimation from 'react-animate-on-scroll'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 import Step from '@components/Step/Step'
+import feedback1 from '@assets/img/feedback-1.svg'
+import feedback2 from '@assets/img/feedback-2.svg'
+import feedback3 from '@assets/img/feedback-4.svg'
 import './Steps.scss'
 
 const Steps = React.memo(({ qualification, url }) => {
@@ -21,11 +26,33 @@ const Steps = React.memo(({ qualification, url }) => {
     <section className="steps">
       <div className="wrapper">
         <div className="steps__container">
-          <h2 className="title steps__title">
-            Шаги: <br />
-            как поступить <br />
-            <span>{qualification}?</span>
-          </h2>
+          <div>
+            <h2 className="title steps__title">
+              Шаги: <br />
+              как поступить <br />
+              <span>{qualification}?</span>
+            </h2>
+            <ScrollAnimation
+              className="step__picture"
+              animateIn="animate__fadeInUp"
+              animateOut="animate__fadeOut"
+              delay={1000}
+              animateOnce>
+              <Parallax
+                className="step__parallax"
+                bgClassName="step__bg-1"
+                bgImage={feedback1}
+                strength={150}>
+                <Parallax
+                  className="step__parallax"
+                  bgClassName="step__bg-2"
+                  bgImage={feedback3}
+                  strength={200}>
+                  <div style={{ height: '400px', width: '100%' }} />
+                </Parallax>
+              </Parallax>
+            </ScrollAnimation>
+          </div>
           <ul className="steps__list">
             {steps.length
               ? steps.map(item => (
